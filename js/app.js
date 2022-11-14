268a207a8e9c12a41f4ccf2a1a334991e3fc013b
DADAKIM <d.ya96v@gmail.com>
2022-11-15 01:19:08 +0900

    :wrench: get_colaData & create_item_list

--- js/app.js
+++ js/app.js
@@ -1,6 +1,9 @@
 const vendingMachine = document.querySelector('.wrap-vend');
+const itemList = vendingMachine.querySelector('.cola-list'); // 자판기 파트 아이템 <ul>
 const leftVal =  vendingMachine.querySelector('.txt-changes'); // 잔액
 const btnChange = vendingMachine.querySelector('.btn-money'); // 거스름돈 반환 버튼
+const smallColaList = vendingMachine.querySelector('.list-take-cola'); // 자판기 파트 장바구니 <ul>
+const itemBtn = vendingMachine.querySelectorAll('.cola-list button'); // 자판기 버튼
 
 const valInput = vendingMachine.querySelector('#inPutMoney'); // 입금액 input
 const btnInput = vendingMachine.querySelector('.btn-put-money'); // 입금버튼
@@ -9,6 +12,42 @@ const btnTake = vendingMachine.querySelector('.btn-take'); // 획득버튼
 const myInfo = document.querySelector('.wrap-info');
 const budget = document.querySelector('#budget'); // 소지금
 
+// 콜라 데이터 불러오기
+
+async function getColaData() {
+    const response = await fetch('./js/item.json');
+    // console.log(response);
+    if(response.ok) {
+        const colaData = await response.json();
+        // console.log(colaData);
+        return colaData;
+        // colaFactory(colaData);
+    } else{
+        alert('실패!!' + response.status);
+    }
+}
+
+// 받은 데이터로 자판기 리스트 뿌려주기
+async function colaFactory(data) {
+    const myCola = await getColaData();
+    // console.log(myCola);
+    const docFrag = document.createDocumentFragment();// 가상돔 빈 객체 생성
+    myCola.forEach(item => {
+        const itemLi = document.createElement('li');
+        const liTemplate = `
+            <button type="button" class="btn-item" data-name="${item.name}" data-count="${item.count}" data-img="${item.img}" data-cost="${item.cost}">
+                <img class="img-item" src="images/${item.img}" alt="${item.name}">
+                <strong class="txt-item">${item.name}</strong>
+                <span class="btn-price">${item.cost}원</span>
+            </button>
+            `
+        itemLi.innerHTML = liTemplate;
+        docFrag.appendChild(itemLi);
+    });
+    itemList.appendChild(docFrag);
+}
+
+colaFactory();
 
 /*
 * 거스름돈 반환
@@ -48,7 +87,7 @@ function inpMoney(event){
 }
 
 
-/** 음료 획득하기
+/**  2. 음료 획득하기
 * 획득 버튼을 누르면 선택한 음료수 목록이 획득한 음료 목록으로 이동합니다.
 * 획득한 음료의 금액을 모두 합하여 총금액을 업데이트 합니다.
 */
@@ -57,8 +96,26 @@ function getItem(){
 }
 
 
+/*
+* 3. 자판기 메뉴 기능
+* 아이템을 누르면 잔액 == 잔액 - 아이템 가격이 됩니다.
+* 아이템 가격보다 잔액이 적다면 "잔액이 부족합니다. 돈을 입금해주세요" 경고창이 나타납니다.
+* 아이템이 획득가능 창에 등록됩니다.
+* 아이템 버튼의 data-count 값이 -1 됩니다.
+* 만약 data-count 값이 0 이라면 부모 li에 sold-out 클래스를 붙여줍니다.
+*/
+itemBtn.forEach((item) => {
+    item.addEventListener('click', (e) =>{
+        const targetEl = e.currentTarget;
+        console.log(targetEl)
+    })
+})
+
+
+
 
 
 btnChange.addEventListener('click', getChange);
 btnInput.addEventListener('click', inpMoney);
 btnTake.addEventListener('click', getItem);
+
